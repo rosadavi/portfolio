613b50d70a8de92deac4bc80c3f0e79de62742d9
rosadavi <davi.rosa.ofc@gmail.com>
2026-05-06 13:48:30 -0300

    return all projects using user id

--- portfolio_api/src/main/java/com/rosadavi/portfolio_api/repository/ProjectRepository.java
+++ portfolio_api/src/main/java/com/rosadavi/portfolio_api/repository/ProjectRepository.java
@@ -3,7 +3,9 @@ package com.rosadavi.portfolio_api.repository;
 import com.rosadavi.portfolio_api.entity.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ProjectRepository extends JpaRepository<Project, UUID> {
+    List<Project> findByUserId(UUID userId);
 }
--- portfolio_api/src/main/java/com/rosadavi/portfolio_api/service/ProjectService.java
+++ portfolio_api/src/main/java/com/rosadavi/portfolio_api/service/ProjectService.java
@@ -3,6 +3,9 @@ package com.rosadavi.portfolio_api.service;
 import com.rosadavi.portfolio_api.entity.Project;
 import com.rosadavi.portfolio_api.repository.ProjectRepository;
 
+import java.util.List;
+import java.util.UUID;
+
 public class ProjectService {
     private final ProjectRepository projectRepository;
 
@@ -12,5 +15,9 @@ public class ProjectService {
 
     public Project save(Project project) {
         return projectRepository.save(project);
+    }
+
+    public List<Project> getProjectByUserId(UUID userId) {
+        return projectRepository.findByUserId(userId);
     }
 }
