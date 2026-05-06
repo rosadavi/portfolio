8c37d3dac2def638e36898db3c4c8fb94b0b34d6
rosadavi <davi.rosa.ofc@gmail.com>
2026-05-06 13:50:48 -0300

    return all experiences using user id

--- portfolio_api/src/main/java/com/rosadavi/portfolio_api/repository/ExperienceRepository.java
+++ portfolio_api/src/main/java/com/rosadavi/portfolio_api/repository/ExperienceRepository.java
@@ -3,7 +3,9 @@ package com.rosadavi.portfolio_api.repository;
 import com.rosadavi.portfolio_api.entity.Experience;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ExperienceRepository extends JpaRepository<Experience, UUID> {
+    List<Experience> findByUserId(UUID userId);
 }
--- portfolio_api/src/main/java/com/rosadavi/portfolio_api/service/ExperienceService.java
+++ portfolio_api/src/main/java/com/rosadavi/portfolio_api/service/ExperienceService.java
@@ -3,6 +3,9 @@ package com.rosadavi.portfolio_api.service;
 import com.rosadavi.portfolio_api.entity.Experience;
 import com.rosadavi.portfolio_api.repository.ExperienceRepository;
 
+import java.util.List;
+import java.util.UUID;
+
 public class ExperienceService {
     private final ExperienceRepository experienceRepository;
 
@@ -12,5 +15,9 @@ public class ExperienceService {
 
     public Experience save(Experience experience) {
         return experienceRepository.save(experience);
+    }
+
+    public List<Experience> getExperienceByUserId(UUID userId) {
+        return experienceRepository.findByUserId(userId);
     }
 }
