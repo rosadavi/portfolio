8543c15ff7c94528e9c933253ea363ed7f8ddff7
rosadavi <davi.rosa.ofc@gmail.com>
2026-05-06 15:27:39 -0300

    feat: route to check if the system is ready

---
+++ portfolio_api/src/main/java/com/rosadavi/portfolio_api/controller/ApiController.java
@@ -0,0 +1,15 @@
+package com.rosadavi.portfolio_api.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+    @GetMapping
+    public ResponseEntity<?> testApi() {
+        return ResponseEntity.ok("Hello World!");
+    }
+}
