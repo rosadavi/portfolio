e7c008be81bfb89f9712d5a5531e12dc50c7d51e
rosadavi <davi.rosa.ofc@gmail.com>
2026-05-06 13:46:42 -0300

    return all stack using user id

--- portfolio_api/src/main/java/com/rosadavi/portfolio_api/repository/StackRepository.java
+++ portfolio_api/src/main/java/com/rosadavi/portfolio_api/repository/StackRepository.java
@@ -3,7 +3,9 @@ package com.rosadavi.portfolio_api.repository;
 import com.rosadavi.portfolio_api.entity.Stack;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface StackRepository extends JpaRepository<Stack, UUID> {
+    List<Stack> findByUserId(UUID userId);
 }
--- portfolio_api/src/main/java/com/rosadavi/portfolio_api/service/StackService.java
+++ portfolio_api/src/main/java/com/rosadavi/portfolio_api/service/StackService.java
@@ -3,6 +3,9 @@ package com.rosadavi.portfolio_api.service;
 import com.rosadavi.portfolio_api.entity.Stack;
 import com.rosadavi.portfolio_api.repository.StackRepository;
 
+import java.util.List;
+import java.util.UUID;
+
 public class StackService {
     private final StackRepository stackRepository;
 
@@ -12,5 +15,9 @@ public class StackService {
 
     public Stack save(Stack stack) {
         return stackRepository.save(stack);
+    }
+
+    public List<Stack> getStackByUser(UUID userId) {
+        return stackRepository.findByUserId(userId);
     }
 }
